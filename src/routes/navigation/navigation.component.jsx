0a69e7a76f501abeb72848c6acd8180d7e7150a2
npxrus <npxrus@yandex.com>
2022-06-27 14:54:57 +0300

    feat: add routing

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,27 @@
+import { Outlet, Link } from 'react-router-dom';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+    <>
+      <header>
+        <nav className='navigation'>
+          <Link className='logo-container' to='/'>
+            <CrwnLogo className='logo' />
+          </Link>
+          <ul className='nav-links-container'>
+            <li className='nav-link'>
+              <Link to='/shop'>Shop</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
