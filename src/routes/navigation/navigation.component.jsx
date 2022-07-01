022b8911cdec48e4e1b45994b10d2c5a6d7767ae
npxrus <npxrus@yandex.com>
2022-07-01 20:21:44 +0300

    feat: add CartIcon component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
@@ -30,6 +31,7 @@ const Navigation = () => {
                 <Link to='/auth'>Sign In</Link>
               </li>
             )}
+            <CartIcon />
           </ul>
         </nav>
       </header>
